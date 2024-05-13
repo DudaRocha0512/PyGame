c40f95974cd2c6033c54c8671c830faac549c150
KaryneAlencar <167936092+KaryneAlencar@users.noreply.github.com>
2024-05-13 14:50:05 -0300

    loop principal1

---
+++ pygame2.py
@@ -0,0 +1,25 @@
+# ----- Loop Principal
+FPS = pygame.time.Clock()
+aceleracao = 0
+while True:
+    FPS.tick(30 + aceleracao)
+
+    for i in range(-1, repete + 1):
+        window.blit(Cenario, (i * compri_cenario + movimento % compri_cenario, 0))
+
+    movimento -= 10
+
+    if abs(movimento) > compri_cenario:
+        movimento = 0
+
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+    
+    aceleracao += 0.01
+
+    sprites.draw(window)
+    sprites.update()
+    pygame.display.flip()
